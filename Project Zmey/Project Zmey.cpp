32f24879d2e1f1acb6eb7248c16d711106ecd642
Alexandr <arrenl@bigmir.net>
2013-11-07 15:32:20 +0200

    fix typo

--- Project Zmey/Project Zmey.cpp
+++ Project Zmey/Project Zmey.cpp
@@ -183,7 +183,7 @@ int main(int argc, char **argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(w,h);
-    glutCreateWindow("Моя змея");
+    glutCreateWindow("My Snake");
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0,w,0,h);
